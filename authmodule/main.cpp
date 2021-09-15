c8a8464d5ee68fc4355d7637fcf8ba03871cb772
Buddy <BuddyBoi@users.noreply.github.com>
2021-09-15 19:40:04 -0400

    Delete authmodule directory

--- authmodule/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-#include <iostream>
-
-std::string amodule::print()
-{
-	return "string";
-}
--- authmodule/main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-class amodule
-{
-public:
-	static __declspec(dllexport) std::string print();
-};
